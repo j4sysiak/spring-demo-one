1bc70ad956ee8ce6b2f29515652e6037830cf2c7
Jacek <Jacek@BERLIN>
2020-02-19 10:20:57 +0100

    qq

--- spring-demo-one/src/pl/jaceksysiak/springdemo/CricketCoach.java
+++ spring-demo-one/src/pl/jaceksysiak/springdemo/CricketCoach.java
@@ -5,6 +5,10 @@ public class CricketCoach implements Coach {
 	//define a private field for the dependency
 	private FortuneService fortuneService;
 	
+	//add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	 
 	//non-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside non-arg constructor");
@@ -33,6 +37,24 @@ public class CricketCoach implements Coach {
 	public String getDailyFortune() {
 		// use my fortuneService to get a fortune
 		return fortuneService.getFortune();
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
 	}
 
 }
