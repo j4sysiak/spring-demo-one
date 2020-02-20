f5c8aedd4fcef52b145047b75a5ab2eaa958db54
Jacek <Jacek@BERLIN>
2020-02-20 08:12:31 +0100

    11

--- spring-demo-one/src/pl/jaceksysiak/springdemo/TrackCoach.java
+++ spring-demo-one/src/pl/jaceksysiak/springdemo/TrackCoach.java
@@ -24,5 +24,15 @@ public class TrackCoach implements Coach {
 		// use my fortuneService to get a fortune
 		return "Just do it: " + fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");		
+	}
 
 }
