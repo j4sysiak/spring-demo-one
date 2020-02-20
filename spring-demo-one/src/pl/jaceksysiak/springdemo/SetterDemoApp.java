186526dbd67111cf00afe375a712af282935c548
Jacek <Jacek@BERLIN>
2020-02-20 07:17:32 +0100

    ww

--- spring-demo-one/src/pl/jaceksysiak/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/pl/jaceksysiak/springdemo/SetterDemoApp.java
@@ -17,6 +17,10 @@ public class SetterDemoApp {
 		
 		//let's call our new method for fortunes
 		 System.out.println(theCoach.getDailyFortune());
+		 
+		// call our new methods to get the literal values
+		System.out.println(theCoach.getEmailAddress());	
+		System.out.println(theCoach.getTeam());
 		
 		//close the context
 		context.close();
